3a729198f8838a9d4b2108ab49694dbe7a6661c7
riya2802 <riya.r@flitwebs>
2019-07-22 09:48:11 +0530

    add time code

---
+++ Empdailyreport/templatetags/custom_filter.py
@@ -0,0 +1,22 @@
+
+
+
+import datetime
+from django import template
+register = template.Library()
+
+@register.simple_tag
+def get_nowtime():
+	time = datetime.datetime.now().time()
+	lunchtime1 = datetime.datetime.strptime('15:00', "%H:%M").time()
+	lunchtime2 = datetime.datetime.strptime('15:30', "%H:%M").time()
+	ofctime1 = datetime.datetime.strptime('18:30', "%H:%M").time()
+	ofctime2 = datetime.datetime.strptime('19:30', "%H:%M").time()
+	if (time >=lunchtime1 and time<=lunchtime2)  or (time >=ofctime1  and time <=ofctime2):
+	    res = "True"
+
+	else:
+	    
+		res ="False"
+	return res
+
